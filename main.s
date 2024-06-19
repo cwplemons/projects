8922aa4aa25a18775b04a47bceabcc7cab390999
cwplemons <cw.plemons@gmail.com>
2024-06-19 13:43:25 -0700

    Update main.s

--- main.s
+++ main.s
@@ -1,4 +1,4 @@
 MOV 0x2D, W0      ;sets pins 27, 25, 24, and 22 to outputs for MISO, SCL, SDA, and UTx.
-MOV W0, 0x0400    ;puts the I/O direction values in the Port A Configuration - DIR register.
+MOV W0, 0x0401    ;puts the I/O direction values in the Port A Configuration - DIRSET register.
 MOV 0x07, W0      ;sets pins 2, 3, and 4 into outputs for 74HC595.
-MOV W0, 0x0440     ;puts the I/O direction values in the Port C Configuration - DIR register.
+MOV W0, 0x0441    ;puts the I/O direction values in the Port C Configuration - DIRSET register.
