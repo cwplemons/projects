1219c728e1e6dc7ff6d18d2f8df3672aec87450e
cwplemons <cw.plemons@gmail.com>
2024-06-20 08:47:32 -0700

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,11 +1,9 @@
-def byte(arr):
-    if len(arr) > 8:
-        return 255
-    else:
-        return int(''.join(['1' if b else '0' for b in arr]), 2)
-
-def bits(num):
-    if not (0 <= num <= 255):
-        return [1, 1, 1, 1, 1, 1, 1, 1]
-    else:
-        return [int(b) for b in format(num, '08b')]
+class Number:
+    def bits(num):
+        if not (0 <= num <= 255): return [1, 1, 1, 1, 1, 1, 1, 1]
+        else: return [int(b) for b in format(num, '08b')]
+    def byte(arr):
+        if len(arr) > 8: return 255
+        else: return int(''.join(['1' if b else '0' for b in arr]), 2)
+    def hex(num):
+        return f'0x{hex(num)[2:].upper()}'
