e9ec64e9a9b0dead13cf0e663e5922fc44417bef
cwplemons <cw.plemons@gmail.com>
2024-10-17 16:18:10 -0700

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,144 +1,117 @@
-@font-face {
-  font-family: "neue";
-  src: url("neue.ttf");
-}
+const calendarArray = [
+  null, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const monthArray = [
+  null,
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+class Calendar {
+  constructor(elem, offset=0) {
+    this.offset = offset;
+    this.prototype = document.createElement('div');
+    this.monthName = document.createElement('p');
+    this.monthName.classList.add('month-name');
+    this.monthNameIdx = new Date().getMonth() + this.offset+1;
+    if (this.monthNameIdx >= 13) {
+      this.monthNameIdx = this.monthNameIdx - 12;
+    };
+    this.monthName.innerHTML = `${monthArray[this.monthNameIdx]}`;
+    this.prototype.appendChild(this.monthName);
+    if (this.monthStart() > 4) {
+      this.prototype.style.gridTemplateRows += ' 1fr';
+    };
+    this.prototype.classList.add('calendar');
+    for (let i of ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]) {
+      let day = document.createElement('div');
+      day.classList.add('calendar-top');
+      day.innerHTML = `<p>${i}</p>`
+      this.prototype.appendChild(day);
+    };
+    for (let i=0; i < this.monthStart(); i++) {
+      let day = this.createDay(0);
+      day.classList.add('empty');
+      day.innerHTML = '';
+      this.prototype.appendChild(day);
+    };
+    for (let i=1; i < this.daysInMonth()+1; i++) {
+      let day = this.createDay(i);
+      this.prototype.appendChild(day);
+    };
+    elem.appendChild(this.prototype);
+    this.markToday();
+  };
+  createDay(num) {
+    let div = document.createElement('div');
+    div.classList.add('calendar-day');
+    div.innerHTML = `<p>${num.toString()}</p>`;
+    return div;
+  };
+  daysInMonth() {
+    let date = new Date();
+    let dateSelect = [date.getFullYear(), date.getMonth() + this.offset + 1];
+    if (dateSelect[1] >= 13) {
+      dateSelect = [dateSelect[0] + 1, dateSelect[1] - 12]
+    };
+    if (dateSelect[1] === 2 && dateSelect[0] % 4 === 0) {
+      return 29;
+    }
+    else {
+      return calendarArray[dateSelect[1]];
+    }
+  };
+  markToday() {
+    let date = new Date();
+    let monthSelect = date.getMonth()+1;
+    let daySelect = date.getDate();
+    
+    if (this.monthNameIdx === monthSelect) {
+      for (let i of document.querySelectorAll('.calendar-day p')) {
+        if (i.innerHTML == daySelect) {
+          i.parentElement.classList.add('today');
+        };
+      };
+    };
+  };
+  monthStart() {
+    let date = new Date();
+    let dateSelect = [date.getFullYear(), date.getMonth() + this.offset];
+    if (dateSelect[1] >= 13) {
+      dateSelect = [dateSelect[0] + 1, dateSelect[1] - 12]
+    };
+    return new Date(...dateSelect, 1).getDay();
+  };
+  removeSelf() {
+    document.body.removeChild(this.prototype);
+  }
+};
 
-@keyframes rotate {
-  from {transform:rotate(0deg);opacity:0}
-  25% {transform: rotate(180deg)}
-  50% {transform: rotate(360deg);opacity:1}
-  75% {transform: rotate(540deg)}
-  to {transform: rotate(719deg);opacity:0}
-}
+function toggleScreen(screen) {
+  screen.classList.toggle('on');
+  if (screen.classList.contains('on')) {
+    screen.style.display = 'block';
+    setTimeout(() => {
+      screen.style.opacity = 1;
+    }, 50);
+  }
+  else {
+    screen.style.opacity = 0;
+    setTimeout(() => {
+      screen.style.display = 1;
+    }, 150);
+  };
+};
 
-@keyframes pulse {
-  from {opacity: 0}
-  50% {opacity: 1}
-  to {opacity: 0}
-}
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  user-select: none;
-  -webkit-tap-highlight-color: transparent;
-  font-family: "neue";
-  font-weight: bolder;
-}
-
-body {
-  background: rgb(32, 32, 32);
-  color: white;
-  font-size: 1.5rem;
-}
-
-.calendar {
-  display: grid;
-  width: 100%;
-  max-width: 480px;
-  aspect-ratio: 4/3;
-  padding: 1rem;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto auto 1fr 1fr 1fr 1fr 1fr;
-  border: 2px solid white;
-  border-radius: 1rem;
-  margin: .5rem 0;
-}
-
-.calendar-top {
-  text-align: center;
-  padding-bottom: .5rem;
-}
-
-.calendar-day {
-  text-align: right;
-  background: white;
-  border: 2px solid rgb(32, 32, 32);
-  color: rgb(32, 32, 32);
-  padding-top: 8px;
-  padding-right: 8px;
-  border-radius: .75rem;
-  font-size: 1.1rem;
-  height: 3rem;
-  transition: scale 100ms;
-}
-
-.calendar-day.empty {
-  background: transparent;
-}
-
-.calendar-day:not(.empty):active {
-  scale: 1.5;
-}
-
-.month-name {
-  grid-column: 1/-1;
-  padding-bottom: 1rem;
-  text-align: center;
-  font-size: 1.5rem;
-}
-
-.calendar-day.today {
-  background: rgb(255, 224, 0);
-}
-
-.load-screen {
-  position: fixed;
-  display: none;
-  width: 100%;
-  height: 100%;
-  background: black;
-  opacity: 0;
-  transition: opacity 150ms;
-  color: white;
-  font-size: 4rem;
-}
-
-.circle-box {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  width: 30%;
-  aspect-ratio: 1/1;
-  transform: translate(-50%, -50%);
-}
-
-.circle {
-  width: 100%;
-  aspect-ratio: 1/1;
-  border: 1rem solid white;
-  border-color: white transparent transparent transparent;
-  border-radius: 100%;
-  filter: drop-shadow(0 0 8px white);
-  animation: 2s rotate linear infinite;
-}
-
-.load-screen p {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  text-shadow: 0 0 8px white;
-}
-
-.send-screen {
-  position: fixed;
-  display: none;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: black;
-}
-
-.send-screen svg {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  fill: white;
-  width: 8rem;
-  filter: drop-shadow(0 0 8px white);
-  animation: 1.5s pulse linear infinite;
+for (let i=0; i<12; i++) {
+  new Calendar(document.body, i);
 }
